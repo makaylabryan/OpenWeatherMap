20b95df9339bd0e68b40f48d23576eded72b2d16
Makayla Bryan <makaylabryan@gmail.com>
2023-06-20 14:53:46 -0400

    Add project files.

---
+++ OpenWeatherMap/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OpenWeatherMap_Activity
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            Console.WriteLine("");
+        }
+    }
+}
